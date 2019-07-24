f8e1c46e7c80dc6c6b4161dc35c8615584e66995
Calin Blaga <calin.blaga@arobs.com>
2019-07-24 09:21:14 +0200

    test(CHI-839): high level consumer builder
    Signed-off-by: Calin Blaga <calin.blaga@arobs.com>

---
+++ tests/Unit/Kafka/Consumer/KafkaHighLevelConsumerBuilderTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Jobcloud\Messaging\Tests\Unit\Kafka\Consumer;
+
+use Jobcloud\Messaging\Kafka\Consumer\KafkaConsumerBuilderException;
+use Jobcloud\Messaging\Kafka\Consumer\KafkaHighLevelConsumerBuilder;
+use PHPUnit\Framework\TestCase;
+use RdKafka\KafkaConsumer;
+
+/**
+ * @covers \Jobcloud\Messaging\Kafka\Consumer\KafkaHighLevelConsumerBuilder
+ */
+final class KafkaHighLevelConsumerBuilderTest extends TestCase
+{
+
+    /**
+     * @var $kcb KafkaHighLevelConsumerBuilder
+     */
+    protected $kafkaHighLevelConsumerBuilder;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->kafkaHighLevelConsumerBuilder = KafkaHighLevelConsumerBuilder::create();
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreate(): void
+    {
+        self::assertInstanceOf(KafkaHighLevelConsumerBuilder::class, KafkaHighLevelConsumerBuilder::create());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetBroker(): void
+    {
+        self::assertSame($this->kafkaHighLevelConsumerBuilder, $this->kafkaHighLevelConsumerBuilder->setBroker('localhost'));
+
+        $reflectionProperty = new \ReflectionProperty($this->kafkaHighLevelConsumerBuilder, 'broker');
+        $reflectionProperty->setAccessible(true);
+
+        self::assertSame('localhost', $reflectionProperty->getValue($this->kafkaHighLevelConsumerBuilder));
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetTimeout(): void
+    {
+        $timeout = 1000;
+
+        self::assertSame($this->kafkaHighLevelConsumerBuilder, $this->kafkaHighLevelConsumerBuilder->setTimeout($timeout));
+
+        $reflectionProperty = new \ReflectionProperty($this->kafkaHighLevelConsumerBuilder, 'timeout');
+        $reflectionProperty->setAccessible(true);
+
+        self::assertSame($timeout, $reflectionProperty->getValue($this->kafkaHighLevelConsumerBuilder));
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetConfig(): void
+    {
+        $this->kafkaHighLevelConsumerBuilder->setConfig(
+            [
+              'timeout' => 100
+            ]
+        );
+
+        $reflectionProperty = new \ReflectionProperty($this->kafkaHighLevelConsumerBuilder, 'config');
+        $reflectionProperty->setAccessible(true);
+
+        self::assertSame(['timeout' => 100], $reflectionProperty->getValue($this->kafkaHighLevelConsumerBuilder));
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetConsumerGroup(): void
+    {
+        $this->kafkaHighLevelConsumerBuilder->setConsumerGroup('funGroup');
+
+        $reflectionProperty = new \ReflectionProperty($this->kafkaHighLevelConsumerBuilder, 'consumerGroup');
+        $reflectionProperty->setAccessible(true);
+
+        self::assertSame('funGroup', $reflectionProperty->getValue($this->kafkaHighLevelConsumerBuilder));
+    }
+
+    /**
+     * @return void
+     */
+    public function testBuildFail(): void
+    {
+        self::expectException(KafkaConsumerBuilderException::class);
+
+        $this->kafkaHighLevelConsumerBuilder
+            ->setConfig(['group.id' => 'foo'])
+            ->build();
+    }
+
+    /**
+     * @return void
+     */
+    public function testBuildSuccess(): void
+    {
+        $consumer = KafkaHighLevelConsumerBuilder::create()
+            ->setBroker('localhost')
+            ->build();
+
+        self::assertInstanceOf(KafkaConsumer::class, $consumer);
+    }
+}
