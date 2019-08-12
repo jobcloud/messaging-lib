198da1b45069ae41ed9fdde108a0bdb5ddb81510
Nick <nicolas.chiu@jobcloud.ch>
2019-08-12 15:52:27 +0200

    change exception wording

--- src/Kafka/Exception/KafkaConsumerBuilderException.php
+++ src/Kafka/Exception/KafkaConsumerBuilderException.php
@@ -6,6 +6,6 @@ namespace Jobcloud\Messaging\Kafka\Exception;
 
 class KafkaConsumerBuilderException extends \Exception
 {
-    const NO_BROKER_EXCEPTION_MESSAGE = 'No brokers defined to connect to.';
+    const NO_BROKER_EXCEPTION_MESSAGE = 'You need add at least one broker to connect to.';
     const NO_TOPICS_EXCEPTION_MESSAGE = 'No topics defined to subscribe to.';
 }
